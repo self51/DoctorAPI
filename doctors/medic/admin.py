81973427c46c350c55f7c181e596fc263849b6d7
self <ggdiscone@gmail.com>
2022-07-03 16:51:41 +0300

    Added two models and registered them to admin panel

--- doctors/medic/admin.py
+++ doctors/medic/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from medic.models import Direction, Doctor
 
-# Register your models here.
+
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ['name_direction', 'slug', 'sort_number']
+    ordering = ['name_direction', 'sort_number']
+    search_fields = ['name_direction', 'slug']
+
+
+admin.site.register(Direction, DirectionAdmin)
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'birth_date', 'sort_number', 'experience']
+    ordering = ['name', 'sort_number']
+    search_fields = ['name', 'slug', 'experience']
+
+
+admin.site.register(Doctor, DoctorAdmin)
--- doctors/medic/models.py
+++ doctors/medic/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Direction(models.Model):
+    name_direction = models.CharField(blank=True, max_length=30)
+    slug = models.SlugField(max_length=40)
+    sort_number = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name_direction
+
+
+class Doctor(models.Model):
+    name = models.CharField(blank=True, max_length=30)
+    slug = models.SlugField(max_length=40)
+    directions = models.ManyToManyField(
+        Direction,
+        related_name="directions",
+        related_query_name="direction",
+    )
+    description = models.CharField(blank=True, max_length=200)
+    birth_date = models.DateField(max_length=8)
+    experience = models.IntegerField(default=0)
+    sort_number = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
