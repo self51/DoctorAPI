9a78d382a3d7f9b45150f29871cb004054a6716a
self <ggdiscone@gmail.com>
2022-07-04 13:52:19 +0300

    Added ability to filter and sort entities

--- doctors/medic/views.py
+++ doctors/medic/views.py
@@ -2,6 +2,8 @@ from .models import Direction, Doctor
 from .serializers import DirectionSerializer, DoctorSerializer
 
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class DirectionViewSet(viewsets.ModelViewSet):
@@ -10,5 +12,8 @@ class DirectionViewSet(viewsets.ModelViewSet):
 
 
 class DoctorViewSet(viewsets.ModelViewSet):
-    queryset = Doctor.objects.all().order_by('-sort_number')
+    queryset = Doctor.objects.all().order_by('sort_number')
     serializer_class = DoctorSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['directions', 'experience', ]
+    ordering_fields = ['birth_date', 'experience', ]
